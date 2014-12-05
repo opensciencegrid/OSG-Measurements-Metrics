7b8b5ed96c4b23aee8f542ba6fb0bc9a48aa273f
juanfmx2 <juanfmx2@gmail.com>
2014-12-05 14:22:30 -0600

    Script to deploy on local machine with graphtool project

---
+++ local-deploy-with-graphtool.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Build RPM's
+./buildrpms.sh
+cd ../graphtool
+./buildrpms.sh
+cd ../OSG-Measurements-Metrics
+# Get Root Access
+# stop service
+# first uninstall in the correct order
+# install again
+# start service
+echo "Root Access"
+su root -c "service GratiaWeb stop ;
+ rpm -e osg-measurements-metrics-web ; 
+ rpm -e osg-measurements-metrics-db ; 
+ rpm -e graphtool ; 
+
+ cd ../graphtool/dist;
+ rpm -ivh graphtool-0.6.6-$2.noarch.rpm ; 
+ cd ../../OSG-Measurements-Metrics/dist;
+ rpm -ivh osg-measurements-metrics-db-1.2-$1.noarch.rpm ; 
+ rpm -ivh osg-measurements-metrics-web-1.2-$1.noarch.rpm ; 
+ cd .. ; 
+service GratiaWeb start;"
