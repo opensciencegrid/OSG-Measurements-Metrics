5acb0e5e8910ed658527aec18ebf14a4f471a08a
juanfmx2 <juanfmx2@gmail.com>
2015-08-14 06:08:14 -0500

    Google charts data formatting, removal of T2 sites links and update of WLCG links

--- src/gratia/database/sites_periodic_updater.py
+++ src/gratia/database/sites_periodic_updater.py
@@ -11,11 +11,20 @@ class OimSitesPeriodicUpdater(query_handler.PeriodicUpdater):
         for child in dom.getElementsByTagName('ResourceGroup'):
             try:
                 rg_name = child.getElementsByTagName('GroupName')[0].firstChild.data
-                sites.append(rg_name)
-                for child in child.getElementsByTagName('Resource'):
+                rg_added = False
+                for child_res in child.getElementsByTagName('Resource'):
                     try:
-                        r_name = child.getElementsByTagName('Name')[0].firstChild.data
-                        sites.append(r_name)
+                        r_name = child_res.getElementsByTagName('Name')[0].firstChild.data
+                        for child_servs in child_res.getElementsByTagName('Service'):
+                            service_name = child_servs.getElementsByTagName('Name')[0].firstChild.data
+                            if service_name == "CE":
+                                hidden_str = child_servs.getElementsByTagName('hidden')[0].firstChild.data
+                                if hidden_str == "False":
+                                    if not rg_added:
+                                        sites.append(rg_name)
+                                        rg_added = True
+                                    sites.append(r_name)
+                                    continue
                     except:
                         pass
             except:
@@ -38,3 +47,22 @@ class OimCMST2Sites(OimSitesPeriodicUpdater):
         super(OimCMST2Sites, self).__init__(self.url)
 
 oim_cms_t2_sites = OimCMST2Sites()
+
+
+
+class OimWLCGSites(OimSitesPeriodicUpdater):
+
+    def __init__(self):
+        srchUrl = 'OimWLCGSitesUrl'
+        modName = 'OimWLCGSites'
+        print "%s: srchUrl: %s" % (modName, srchUrl)
+        try:
+            self.url = GratiaURLS().GetUrl(srchUrl)
+            print "%s: SUCCESS: GratiaURLS().GetUrl(url = %s)" % (modName,srchUrl)
+            print "%s: retUrl: %s" % (modName, self.url)
+        except:
+            print "%s: FAILED: GratiaURLS().GetUrl(url = %s)" % (modName,srchUrl)
+            pass
+        super(OimWLCGSites, self).__init__(self.url)
+        
+oim_wlcg_sites = OimWLCGSites()
