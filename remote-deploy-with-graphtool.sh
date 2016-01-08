180db7bc5aabb34ac70a74f75c284301dc9676b0
juanfmx2 <juanfmx2@gmail.com>
2016-01-08 17:37:49 -0600

    Modifications to include html pages instead of xml/xslt

--- remote-deploy-with-graphtool.sh
+++ remote-deploy-with-graphtool.sh
@@ -2,26 +2,33 @@
 
 echo "Root Access on $1"
 kinit
-# Build and copy RPM's
-./buildrpms.sh
-scp ./dist/osg-measurements-metrics-db-1.4-$2.noarch.rpm root@$1:~/
-scp ./dist/osg-measurements-metrics-web-1.4-$2.noarch.rpm root@$1:~/
-cd ../graphtool
-./buildrpms.sh
-scp ./dist/graphtool-0.8.0-$3.noarch.rpm root@$1:~/
-cd ../OSG-Measurements-Metrics
-# Get Root Access
-# stop service
-# first uninstall in the correct order
-# install again
-# start service
-ssh root@$1 "service GratiaWeb stop ;
- rpm -e osg-measurements-metrics-web ; 
- rpm -e osg-measurements-metrics-db ; 
- rpm -e graphtool ; 
- cd ~/;
-rpm -ivh graphtool-0.8.0-$3.noarch.rpm ;
-rpm -ivh osg-measurements-metrics-db-1.4-$2.noarch.rpm ; 
-rpm -ivh osg-measurements-metrics-web-1.4-$2.noarch.rpm ; 
- cd .. ; 
-service GratiaWeb start;"
+if [ $? -eq 0 ]; then
+  # Build and copy RPM's
+  ./buildrpms.sh
+  scp ./dist/osg-measurements-metrics-db-1.4-$2.noarch.rpm root@$1:~/
+  scp ./dist/osg-measurements-metrics-web-1.4-$2.noarch.rpm root@$1:~/
+  cd ../graphtool
+  ./buildrpms.sh
+  scp ./dist/graphtool-0.8.0-$3.noarch.rpm root@$1:~/
+  cd ../OSG-Measurements-Metrics
+  # Get Root Access
+  # stop service
+  # first uninstall in the correct order
+  # install again
+  # start service
+  ssh root@$1 "service GratiaWeb stop ;
+   rpm -e osg-measurements-metrics-web ; 
+   rpm -e osg-measurements-metrics-db ; 
+   rpm -e graphtool ; 
+   cd ~/;
+   rpm -ivh graphtool-0.8.0-$3.noarch.rpm ;
+   rpm -ivh osg-measurements-metrics-db-1.4-$2.noarch.rpm ; 
+   rpm -ivh osg-measurements-metrics-web-1.4-$2.noarch.rpm ; 
+   rpm -q graphtool;
+   rpm -q osg-measurements-metrics-db; 
+   rpm -q osg-measurements-metrics-web; 
+   cd .. ; 
+   service GratiaWeb start;"
+else
+  echo KINIT FAILED
+fi
