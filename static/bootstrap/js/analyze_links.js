428b88dd6a4f5783d9830c9d87ca764ee0f74f48
juanfmx2 <juanfmx2@gmail.com>
2016-06-29 23:31:07 -0500

    Changes to start service and start Xvb service to support image generation of html pages with selenium, update to use google charts in the main pages instead of matplotlib

--- static/bootstrap/js/analyze_links.js
+++ static/bootstrap/js/analyze_links.js
@@ -3,7 +3,7 @@ var gratia_regex = /\/gratia\/.*?\//
 var static_gratia_regex = /\/gratia\/(today)|(static)/
     
 function replace_url_to_analize(url_param){
-    return url_param.replace(gratia_regex,'/gratia/xml/')
+    return url_param.replace(gratia_regex,'/gratia/html/')
 }
 function replace_url_to_csv(url_param){
     return url_param.replace(gratia_regex,'/gratia/csv/')
@@ -13,9 +13,25 @@ function endsWith(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-$('img').each(function(index,e){
+function resizeIframe(ifrm) {
+  setTimeout(function(){
+    ifrm.style.width = ifrm.contentWindow.document.body.scrollWidth + 'px';
+    var total_height = 0
+    total_height += $('#gc_title_div', ifrm.contentWindow.document).height();
+    console.log(total_height);
+    total_height += $('#gc_chart_div', ifrm.contentWindow.document).height();
+    console.log(total_height);
+    total_height += $('#gc_legend_div', ifrm.contentWindow.document).height();
+    console.log(total_height);
+    ifrm.style.height=total_height + "px";
+  },1000);
+}
+
+$('iframe').each(function(index,e){
     if(e.src && e.src.match(gratia_regex) && !e.src.match(static_gratia_regex)){
-        var str ='<br/>(<a href="'+replace_url_to_analize(e.src)+'">Analyze</a>)(<a href="'+replace_url_to_csv(e.src)+'">View raw data</a>)<hr/>'
-        $(e).after(str)
+        resizeIframe(e);
+        var new_url = e.src.replace("&no_html_frame=y","");
+        var str ='<br/>(<a href="'+replace_url_to_analize(new_url)+'">Analyze</a>)(<a href="'+replace_url_to_csv(new_url)+'">View raw data</a>)<hr/>';
+        $(e).after(str);
     }
 })
